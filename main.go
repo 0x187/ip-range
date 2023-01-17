af7c98f98f562fa58eda684487a74024bfe56571
Sr <64494086+0x187@users.noreply.github.com>
2023-01-17 16:30:00 +0330

    Add files via upload

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func inc(ip net.IP) {
+	for j := len(ip) - 1; j >= 0; j-- {
+		ip[j]++
+		if ip[j] > 0 {
+			break
+		}
+	}
+}
+
+func main() {
+	var input string
+	flag.StringVar(&input, "ip", "", "")
+	flag.Parse()
+
+	f, err := os.Create("data.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	ip, ipnet, err := net.ParseCIDR(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); inc(ip) {
+		fmt.Printf("%s \n", ip)
+
+		_, err := f.WriteString(ip.String() + "\n")
+
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
